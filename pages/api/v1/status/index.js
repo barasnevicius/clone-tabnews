d1ae73deb8eaa99b63ecf3b6cedb688451952965
zamorano <42423102+barasnevicius@users.noreply.github.com>
2024-03-13 19:32:26 +0000

    Finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 10+20 AS SOMA;");
-  console.log(result.rows);
-  res.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString(); // Get the current date and time
+
+  const dbVersionResult = await database.query("SHOW server_version;"); // Database version
+  const dbVersion = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW MAX_CONNECTIONS;"); // Max. number of connections that is allowed
+  const dbMaxConnections = dbMaxConnectionsResult.rows[0].max_connections;
+
+  // SQL injection prevention (database name by environment variable)
+  const databaseName = process.env.POSTGRES_DB;
+  const usedLocalConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const usedLocalConnections = usedLocalConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(dbMaxConnections),
+        opened_local_connections: usedLocalConnections,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+  expect(response.status).toBe(200); // Verify database and API are running
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined(); // Verify updated_at variable exists
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).toEqual(responseBody.updated_at); // Verify updated_at is a valid date
+
+  expect(responseBody.dependecies.database.version).toEqual("16.0");
+  expect(responseBody.dependecies.database.max_connections).toEqual(100); // Verify max_connections
+  expect(responseBody.dependecies.database.opened_local_connections).toEqual(1); // Verify used_connections is 1 (when local)
 });
